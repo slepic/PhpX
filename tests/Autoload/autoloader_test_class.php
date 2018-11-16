f631d2d3c2c28b2f09feaf8c051a4bb6f567995e
Live session user <gslepic@gmail.com>
2018-11-16 21:21:18 +0000

    added test run script and Autoloader test

---
+++ tests/Autoload/autoloader_test_class.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace NonexistentNamespace;
+
+class NonexistentClass {}
---
+++ tests/run.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+path=`readlink -f $0`
+path=`dirname $path`
+path=`dirname $path`
+
+"$path/vendor/phpunit/phpunit/phpunit" --bootstrap "$path/vendor/autoload.php" "$path/tests"
+
