59bae90480a08759673cb42c5fb44b2dccb2ff2f
slepic <glepic@gmail.com>
2018-12-21 23:58:43 +0000

    ...

--- tests/CommandPattern/Undo/UndoableInvokerTest.php
+++ tests/CommandPattern/Undo/UndoableInvokerTest.php
@@ -4,6 +4,7 @@ namespace PhpX\Tests\CommandPattern\Undo;
 
 use PHPUnit\Framework\TestCase;
 use PhpX\CommandPattern\Undo\UndoableInvoker;
+use PhpX\CommandPattern\Undo\UndoableCommandInterface as UndoableCommand;
 use PhpX\CommandPattern\InvokerInterface as Invoker;
 use PhpX\Collections\StackInterface as Stack;
 use PhpX\Tests\CommandPattern\InvokerInterfaceTestTrait;
@@ -46,5 +47,23 @@ class UndoableInvokerTest extends TestCase
         $invoker = new UndoableInvoker($fallback, $stack);
         $this->assertSame($fallback, $invoker->getInnerInvoker());
         $this->assertSame($stack, $invoker->getUndoStack());
+    }
+
+    public function testCanExecuteCommandAndPassTheStackToAnotherInvoker()
+    {
+        $command = $this->createMock(UndoableCommand::class);
+        $invoker = new UndoableInvoker();
+        $this->assertFalse($invoker->canUndo());
+        $invoker->executeCommand($command);
+        $this->assertTrue($invoker->canUndo());
+
+        //serialize stack, end request, start new request, unserialize stack
+        $stack = $invoker->getUndoStack();
+
+        $invoker2 = new UndoableInvoker(null, $stack);
+        $this->assertTrue($invoker2->canUndo());
+        $invoker2->undo();
+        $this->assertFalse($invoker2->canUndo());
+        $this->assertFalse($invoker->canUndo());
     }
 }
